f34c2603cabe9aadc14630b8c457b25a1a726e4b
XenoAmess <912890489@qq.com>
2018-04-22 01:09:46 +0800

    1
    1

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ screen -S exsimple -X quit
 autoreconf -vfi
 
 mkdir /SpeedyExsimple/environments
-mkdir /SpeedyExsimple/FILE
+mkdir /SpeedyExsimple/files
 
 cd /SpeedyExsimple/environments
 git clone https://github.com/openssl/openssl
@@ -22,10 +22,14 @@ cd /SpeedyExsimple
 ./auto/configure --prefix=/SpeedyExsimple --conf-path=/SpeedyExsimple/nginx.conf --with-pcre=/SpeedyExsimple/environments/pcre-8.42 --with-zlib=/SpeedyExsimple/environments/zlib --with-http_ssl_module --with-openssl=/SpeedyExsimple/environments/openssl --with-http_geoip_module
 make
 make install
+
+rm /SpeedyExsimple/nginx.conf
+cp /SpeedyExsimple/SpeedyExsimple.conf /SpeedyExsimple/nginx.conf
+
 nginx
- 
+
 cd /SpeedyExsimple
 wget -O /SpeedyExsimple/src/exsimple.py https://raw.githubusercontent.com/XenoAmess/EXsimple/master/src/exsimple.py
 sed -i "s/THIS_IS_DAILYPASTE = False;/THIS_IS_DAILYPASTE = True;/g" /SpeedyExsimple/src/exsimple.py
 nuitka --standalone  --recurse-all  --python-version=3.5  /SpeedyExsimple/src/exsimple.py  --output-dir=/SpeedyExsimple/sbin/  --remove-output
-sudo screen -dmS exsimple sudo /SpeedyExsimple/sbin/exsimple.dist/exsimple.exe dir=/SpeedyExsimple/
+sudo screen -dmS exsimple sudo /SpeedyExsimple/sbin/exsimple.dist/exsimple.exe dir=/SpeedyExsimple/files/ port=7777
